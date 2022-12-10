5a1f7b4a9d55785399130be02386d94e8604d796
Abdellatif Al Kerde <boudikerde0@gmail.com>
2022-12-10 13:03:22 +0200

    Basic Queries

---
+++ answers.sh
@@ -0,0 +1,14 @@
+Basic Queries:
+
+1- SELECT * FROM students
+2- SELECT * FROM students where Age > 30
+3- SELECT * FROM students WHERE Age = 30 and Gender = "F"
+4- SELECT Points FROM students WHERE name = "Alex"
+5- INSERT INTO students (name, Age, Gender, Points)
+VALUES ("Abdellatif", "21", "M", 350)
+6- UPDATE students 
+SET Points = 350
+WHERE name = "Basma"
+7- UPDATE students
+SET Points = 150
+WHERE name = "Alex"
